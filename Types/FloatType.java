c4f0fc360bca514ff1d464a8a60b0fb3e0bccdf2
Nicholas Kobald <nick.kobald@gmail.com>
2018-01-18 23:40:37 -0800

    Remove random file

---
+++ Types/FloatType.java
@@ -0,0 +1,11 @@
+package types; 
+
+
+public class FloatType extends BaseType {
+
+    String tok; 
+
+    public FloatType(String t) {
+        this.tok  = t; 
+    }
+}
---
+++ Types/VoidType.java
@@ -0,0 +1,11 @@
+package types; 
+
+
+public class VoidType extends BaseType {
+
+    String tok; 
+
+    public VoidType(String t) {
+        this.tok  = t; 
+    }
+}
