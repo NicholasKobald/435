d7d4b17762916ab62adb4ee11a637fcec7c8ede5
Nicholas <nickk@linux6.csc.uvic.ca>
2018-03-17 14:07:05 -0700

    implement bash script for testing things

--- test_on_linux.sh
+++ test_on_linux.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-ir_input_file=$1 
+ul_input_file=$1 
+
+echo '* Running compiler on ul file'
+java Compiler $ul_input_file > 'out.ir' 
 
 echo '* Generating jasmin file "out.j"'
-/home/jcorless/csc435/codegen/codegen --file=$ir_input_file > out.j
+/home/jcorless/csc435/codegen/codegen --file='out.ir'> out.j
 
 echo '* Generating class file'
 java jasmin.Main out.j
 
-echo '* Running program!'
-# TODO: parametrize classname
-java classname
+echo '* Running program!\n'
 
+java myclassname 
+
+echo '\n* Done' 
